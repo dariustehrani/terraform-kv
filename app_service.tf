615351fb45b8c186308a07951197f8268096150c
Darius Tehrani <darius.tehrani@microsoft.com>
2019-06-12 15:48:33 +0000

    fixed docker image ref

--- app_service.tf
+++ app_service.tf
@@ -5,11 +5,13 @@ resource "azurerm_app_service_plan" "app-service-plan" {
   location            = "${var.location}"
   resource_group_name = "${azurerm_resource_group.infra.name}"
   kind                = "Linux"
+  reserved            = true
 
-  sku {
-    tier = "Premium"
+sku {
+    tier = "PremiumV2"
     size = "P1v2"
-  }
+
+}
 
   tags = "${var.tags}"
 }
@@ -18,20 +20,21 @@ resource "azurerm_app_service" "app" {
   name                      = "${var.project_name}-${random_id.project-id.hex}"
   location                  = "${var.location}"
   resource_group_name       = "${azurerm_resource_group.infra.name}"
-  app_service_plan_id       = "${azurerm_app_service_plan.app-service-plan}"
+  app_service_plan_id       = "${azurerm_app_service_plan.app-service-plan.id}"
 
   identity {
     type = "SystemAssigned"
   }
 
   site_config {
-    linux_fx_version = "DOCKER|dariustehrani/terraform-kv:latest"
+    linux_fx_version = "DOCKER|dariustehrani/demo-app:latest"
   }
 
 
   app_settings = {
     "KEY_VAULT_URI" = "${azurerm_key_vault.kv.vault_uri}"
-  }
+    "DOCKER_REGISTRY_SERVER_URL" = "https://index.docker.io"
+}
 
 
   tags = "${var.tags}"
