d8370040a6a85045c3a790a853731227f6916642
Darius Tehrani <darius.tehrani@microsoft.com>
2019-06-12 13:50:36 +0200

    interim

--- demo_app/app.js
+++ demo_app/app.js
@@ -13,7 +13,7 @@ const app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+app.set("view engine", "pug");
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -45,7 +45,7 @@ taskDao
 app.get("/", (req, res, next) => taskList.showTasks(req, res).catch(next));
 app.post("/addtask", (req, res, next) => taskList.addTask(req, res).catch(next));
 app.post("/completetask", (req, res, next) => taskList.completeTask(req, res).catch(next));
-app.set("view engine", "jade");
+app.set("view engine", "pug");
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
