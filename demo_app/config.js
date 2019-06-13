b3539cde8fb819cb946b5e19be6340f0e13683a4
Darius Tehrani <darius.tehrani@microsoft.com>
2019-06-13 13:26:16 +0200

    package-lock

--- demo_app/config.js
+++ demo_app/config.js
@@ -22,21 +22,14 @@ function getKeyVaultCredentials(){
 var cosmosdbsecret =
 function getKeyVaultSecret(credentials) {
   let keyVaultClient = new KeyVault.KeyVaultClient(credentials);
-  return keyVaultClient.getSecret(KEY_VAULT_URI, 'cosmosdbsecret', "") || "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==";
+  return keyVaultClient.getSecret(KEY_VAULT_URI, 'cosmosdbsecret', "");
 }
 
 
-config.host = process.env['COSMOSDB_URI'] || "https://localhost:8081/";
+config.host = process.env['COSMOSDB_URI'];
 config.authKey = (cosmosdbsecret)
 config.databaseId = "ToDoList";
 config.containerId = "Items";
 
-
-if (config.host.includes("https://localhost:")) {
-  console.log("Local environment detected");
-  console.log("WARNING: Disabled checking of self-signed certs. Do not have this code in production.");
-  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-  console.log(`Go to http://localhost:${process.env.PORT || '3000'} to try the sample.`);
-}
 
 module.exports = config;
